9b065f7ca85c8296666677aa67ec72e4939c69db
Niladrim14 <niladrim004@gmail.com>
2025-11-12 18:41:22 +0530

    Finally done

--- frontend/src/components/Todo.jsx
+++ frontend/src/components/Todo.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import React,{useRef,useEffect} from 'react'
 import todo_icon from '../assets/todo_icon.png'
 import TodoItems from './TodoItems.jsx'
 
 const Todo = () => {
+
+  const [todolist, setTodolist] = React.useState(JSON.parse(localStorage.getItem('todolist')) || []);
+  const inputRef = useRef();
+
+const add = () => {
+  const value = inputRef.current.value.trim();
+         if (value === '') return;
+     const newTodo = { id: Date.now(), text: value, completed: false };
+
+     setTodolist((prev)=>[...prev,newTodo]);
+         inputRef.current.value = '';
+   };
+
+   const deletetodo = (id) => {
+    setTodolist((prev) => { return prev.filter((todo) => todo.id !== id) });
+  };
+     const completetodo = (id) => {
+      setTodolist((prev) => {
+        return prev.map((todo) => {
+          if (todo.id === id) {
+            return { ...todo, completed: !todo.completed };
+          }
+          return todo;
+        });
+      });
+    };
+  useEffect(() => { 
+    localStorage.setItem('todolist',JSON.stringify(todolist));
+    
+    
+  }, [todolist]);
   return (
     <div className='bg-white p-6 rounded-lg shadow-lg w-11/12 max-w-md  flex flex-col py-7 min-h-[500px]'>
          <div className='flex ml-25 items-center  '>
@@ -11,13 +42,16 @@ const Todo = () => {
          </div>
                        {/* Input Section */}
         <div className='flex items-center my-7 bg-gray-200 rounded-full'>
-            <input className='bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-500 text-black' type="text" placeholder='Add your Task ' />
-            <button className='bg-orange-600 rounded-md text-white border  px-4 py-2 cursor-pointer'> ADD +</button>
+            <input  ref={inputRef}   className='bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-500 text-black' type="text" placeholder='Add your Task ' />
+            <button onClick={add} className='bg-orange-600 rounded-md text-white border  px-4 py-2 cursor-pointer'
+            > ADD +</button>
         </div>
         {/* Todo List*/}
         <div>
-            <TodoItems text="Learn Coding" />
-            <TodoItems text="Learn Coding from YouTube" />
+           {todolist.map((item)=>(
+            <TodoItems key={item.id} id={item.id} text={item.text} isCompleted={item.completed} deletetodo={deletetodo} completetodo={completetodo} />
+           ))}
+           
         </div>
     </div>
   )
--- frontend/src/components/TodoItems.jsx
+++ frontend/src/components/TodoItems.jsx
@@ -1,16 +1,17 @@
-import React from 'react'
+
 import tick from '../assets/tick.png'
 import not_tick from '../assets/not_tick.png'
 import delete_icon from '../assets/delete.png'
 
-const TodoItems = ({ text }) => {
+const TodoItems = ({ text , isCompleted, deletetodo,id,completetodo }) => {
   return (
     <div className='flex items-center my-3 gap-2 '>
      <div className='flex flex-1 items-center cursor-pointer'>
-        <img src={tick} alt="Tick" />
-        <p className='text-slate-700 ml-4 text-[17px]'>{text}</p>
+        <img onClick={()=>{completetodo(id)}} src={isCompleted ? tick : not_tick} alt="Tick" className='w-5 h-5 cursor-pointer' />
+
+        <p className={`text-slate-600 ml-4 text-[17px] decoration-slate-900 ${isCompleted ? 'line-through' : ''}`}>{text}</p>
      </div>
-     <img src={delete_icon} alt="Delete" className='w-5 h-5 cursor-pointer' />
+     <img onClick={() => {deletetodo(id)}} src={delete_icon} alt="Delete" className='w-5 h-5 cursor-pointer' />
     </div>
   )
 }
